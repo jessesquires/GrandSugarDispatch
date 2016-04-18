80e95b0fa855e762cb4177f8b6217c698a15c8dd
Bas Broek <BasThomas@users.noreply.github.com>
2016-04-18 19:58:44 +0200

    Using leading dot syntax for every switch case
    Also removed an abbreviation (and now use it's full name) because why not?

--- Source/GrandSugarDispatch.swift
+++ Source/GrandSugarDispatch.swift
@@ -41,17 +41,17 @@ public enum DispatchQueue {
 
     var dispatchQueue: dispatch_queue_t {
         switch self {
-        case main:
+        case .main:
             return dispatch_get_main_queue()
-        case userInteractive:
+        case .userInteractive:
             return dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0)
-        case userInitiated:
+        case .userInitiated:
             return dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
-        case regular:
+        case .regular:
             return dispatch_get_global_queue(QOS_CLASS_DEFAULT, 0)
-        case utility:
+        case .utility:
             return dispatch_get_global_queue(QOS_CLASS_UTILITY, 0)
-        case background:
+        case .background:
             return dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)
         case .concurrent(let name):
             return dispatch_queue_create(name, DISPATCH_QUEUE_CONCURRENT)
@@ -73,7 +73,7 @@ public func dispatch(queue queue: DispatchQueue, concurrency: Concurrency = .asy
         dispatch_barrier_async(queue.dispatchQueue, closure)
     case .barrierSync:
         dispatch_barrier_sync(queue.dispatchQueue, closure)
-    case .delay(let d):
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(d * Double(NSEC_PER_SEC))), queue.dispatchQueue, closure)
+    case .delay(let delay):
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(delay * Double(NSEC_PER_SEC))), queue.dispatchQueue, closure)
     }
 }
